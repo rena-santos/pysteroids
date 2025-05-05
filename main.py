0f4274c2ed6de29469f2d598ed034dccb2d54f90
rena-santos <rena.santos.dev@gmail.com>
2025-05-05 12:00:05 +0900

    Basic setup - main.py and constants

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+import constants
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width: " + str(constants.SCREEN_WIDTH))
+    print("Screen height: " + str(constants.SCREEN_HEIGHT))
+
+
+if __name__ == "__main__":
+    main()
