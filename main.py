b688faa716733774b0109ea4e44cb18f4aacaa81
rena-santos <rena.santos.dev@gmail.com>
2025-05-06 10:57:32 +0900

    FPS limiter and basic game object shape

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pygame
 
-import constants
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width: " + str(constants.SCREEN_WIDTH))
-    print("Screen height: " + str(constants.SCREEN_HEIGHT))
+    pygame.init()
+    pygame.display.set_caption("Pysteroids")
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+
+        #Refresh the screen after everything was updated
+        pygame.display.flip()
+
+        #Wait for next frame time
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
